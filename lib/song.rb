dde4e8fbb9d734e229aa6b31df226b6ef593fe10
Joshua M Loran <joshua.loran@gmail.com>
2019-02-21 05:35:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,9 @@ class Song
     @@genres << genre
     @@genre_count = {}
     if !@@genre_count.has_key?(genre)
-      
+      @@genre_count[genre] = 1
+    else
+      @@genre_count[genre] += 1
     end
   end
   
